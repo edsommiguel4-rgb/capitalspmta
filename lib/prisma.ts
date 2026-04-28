aa239f782162897b5c9a8c465af14da28eee20ca
edsommiguel4-rgb <edsommiguel4@gmail.com>
2026-04-28 19:54:35 -0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { ensureDb } from "./db-init";
 
-ensureDb(); // 🔥 FORÇA criar as tabelas
-
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
